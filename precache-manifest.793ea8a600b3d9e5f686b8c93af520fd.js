60e0a980c7b948270834c33ec1c26dc122043234
Vineethcosta <reddyvineeth6@gmail.com>
2018-11-25 12:04:09 +0530

    Updates

---
+++ precache-manifest.793ea8a600b3d9e5f686b8c93af520fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mcqedu/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "62ea3772d26698fa2c07",
+    "url": "/mcqedu/static/js/runtime~main.62ea3772.js"
+  },
+  {
+    "revision": "21ccd88397428686a6df",
+    "url": "/mcqedu/static/js/main.21ccd883.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/mcqedu/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "21ccd88397428686a6df",
+    "url": "/mcqedu/static/css/main.fcb5a216.chunk.css"
+  },
+  {
+    "revision": "d55c06f949224be38a9958a56c95733a",
+    "url": "/mcqedu/index.html"
+  }
+];
